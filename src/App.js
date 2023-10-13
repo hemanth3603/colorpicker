1b87cd8f8f5a5506468a021d913cd3c6055188c3
Hemanth <hemu3603@gmail.com>
2023-10-13 16:52:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Colorpicker from './components/Colorpicker';
+function App() {
+  return (
+    <div > 
+      <Colorpicker></Colorpicker>
+    </div>
+  );
+}
+
+export default App;
